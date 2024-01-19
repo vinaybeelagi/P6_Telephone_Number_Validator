151184b49f9bc607b8acd2ced11eac06eeb6e4da
vinaybeelagi <vinaykumar.b@neoito.com>
2024-01-19 09:49:52 +0530

    Get references to HTML elements

---
+++ script.js
@@ -0,0 +1,5 @@
+// Get references to HTML elements
+const userInput = document.getElementById('user-input');
+const checkBtn = document.getElementById('check-btn');
+const clearBtn = document.getElementById('clear-btn');
+const resultsDiv = document.getElementById('results-div');
